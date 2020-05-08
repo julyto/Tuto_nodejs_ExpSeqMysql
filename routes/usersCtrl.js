63955681d525a1ecdaf6d9da0d8b16cdacdfd0c5
julyto <julien.guillemineau@gmail.com>
2020-05-08 22:23:05 +0200

    mise en place des routes et ajout de l'enregistement d'un utilisateur en bdd

---
+++ routes/usersCtrl.js
@@ -0,0 +1,51 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const models = require('../models')
+
+module.exports = {
+    register: (req, res) => {
+
+        var email = req.body.email
+        var username = req.body.username
+        var password = req.body.password
+        var bio = req.body.bio
+
+        if(email == null || username == null || password == null)  {
+            return res.status(400).json({ 'error': 'missing parameters' })
+        }
+
+        models.User.findOne({
+            attributes: ['email'], 
+            where: { email: email }
+        })
+        .then(function(userFound) {
+            if(!userFound) {
+                bcrypt.hash(password, 5, function( err, bcryptedPassword) {
+                    var newUser = models.User.create({
+                        email: email,
+                        username: username,
+                        password: bcryptedPassword,
+                        bio: bio,
+                        isAdmin: 0
+                    })
+                    .then(function(newUser) {
+                        return res.status(201).json({ 'userId': newUser.id})
+                    })
+                    .catch(function(err) {
+                        return res.status(500).json({ 'error': 'cannot add user' })
+                    })
+                })
+
+            } else {
+                return res.status(409).json({ 'error': 'user already exist' })
+            }
+
+        })
+        .catch(function(err) {
+            return res.status(500).json({ 'error': 'unable to verify user' })
+        })
+    },
+    login: (req, res) => {
+
+    }
+}
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const apiRouter = require('./apiRouter').router
 
 var app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+app.use('/api/', apiRouter)
 
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html')
